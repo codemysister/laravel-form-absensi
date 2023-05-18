b6c4ea43ba3588ded3e2191f54ac0faa56d46c0d
Deva <theprojessa@gmail.com>
2023-05-18 20:11:02 +0700

    setup route auth admin

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/dashboard', function () {
-    return view('admin.dashboard');
+// ADMIN DASHBOARD
+Route::prefix('admin')->group(function(){
+
+    // Auth
+    Route::get('/login', function(){
+        return view('admin.login');
+    });
+
+    // Master Data
+    Route::get('/dashboard', function () {
+        return view('admin.dashboard');
+    });
+
 });
 
